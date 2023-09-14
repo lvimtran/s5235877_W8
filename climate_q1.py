c108f4268e8fc1a52656a93d9390ea925ec9edd9
Lily <lvmytran@gmail.com>
2023-09-14 14:45:14 +1000

    Adding connection

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,23 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM employee")
+
 years = []
 co2 = []
 temp = []
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_1.png")
