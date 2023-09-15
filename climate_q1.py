6054293d83e3d5cc7fdcd6ec9663ae6302f9e1b1
Lily <lvmytran@gmail.com>
2023-09-15 10:40:39 +1000

    Done Q1 and Q2

--- climate_q1.py
+++ climate_q1.py
@@ -1,13 +1,14 @@
 import sqlite3
 import matplotlib.pyplot as plt
 
-connection = sqlite3.connect("climate.db")
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM employee")
+conn = sqlite3.connect("climate.db")
+cursor = conn.cursor()
+cursor.execute("SELECT year, co2, temperature FROM climate_data")
+data = cursor.fetchall()
 
-years = []
-co2 = []
-temp = []
+years = [row[0] for row in data]
+co2 = [row[1] for row in data]
+temp = [row[2] for row in data]
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--')
